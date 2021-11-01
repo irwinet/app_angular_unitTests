02aaf46960d65b7a88da9942dfa5a2e778b1eb06
irwinet <irwinestradatorres@gmail.com>
2021-10-31 22:26:00 -0500

    Html Component

--- src/app/intermedio2/incrementador/incrementador.component.spec.ts
+++ src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { IncrementadorComponent } from './incrementador.component';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 
 describe('Incremendator Component', () => {
@@ -8,10 +9,10 @@ describe('Incremendator Component', () => {
     let component: IncrementadorComponent;
     let fixture: ComponentFixture<IncrementadorComponent>;
 
-    beforeEach( () => {
+    beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [ IncrementadorComponent ],
-            imports: [ FormsModule ]
+            declarations: [IncrementadorComponent],
+            imports: [FormsModule]
         });
 
         fixture = TestBed.createComponent(IncrementadorComponent);
@@ -19,9 +20,26 @@ describe('Incremendator Component', () => {
 
     });
 
-    it('', () => {
+    it('Debe de mostrar la leyenda', () => {
 
+        component.leyenda = 'Progreso de carga';
+        fixture.detectChanges(); // disparar la deteccion de cambios
 
+        const elem: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
+
+        expect(elem.innerHTML).toContain('Progreso de carga');
+    });
+
+    it('Debe de mostrar en el input el valor del progreso', () => {
+        component.cambiarValor(5);
+
+        fixture.detectChanges();
+
+        fixture.whenStable().then(()=>{
+            const input = fixture.debugElement.query(By.css('input')).nativeElement;
+
+            expect(input.value).toBe('55');
+        });    
     });
 
 });
