fe77d68b3d3aebd81344bebc8f255931736f562a
irwinet <irwinestradatorres@gmail.com>
2021-11-01 00:39:13 -0500

    Testing Rutas Directivas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { RouterLinkWithHref, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './avanzado/navbar/navbar.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
+        // NavbarComponent
       ],
+      imports: [
+        RouterTestingModule.withRoutes([])
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   });
 
@@ -22,10 +34,11 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('pruebasApp');
   });
 
-  it('should render title', () => {
+  it('Debe de tener un router-outlet', ()=>{
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pruebasApp app is running!');
-  });
+    const debugElement = fixture.debugElement.query(By.directive(RouterOutlet));
+
+    expect(debugElement).not.toBeNull();
+  });  
+
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { MedicoService } from './intermedio2/medico/medico.service';
 import { HospitalComponent } from './intermedio2/hospital/hospital.component';
 import { IncrementadorComponent } from './intermedio2/incrementador/incrementador.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { RUTAS } from './avanzado/rutas/app.routes';
+import { NavbarComponent } from './avanzado/navbar/navbar.component';
 
 
 @NgModule({
@@ -17,12 +20,14 @@ import { FormsModule } from '@angular/forms';
     MedicosComponent,
     MedicoComponent,
     HospitalComponent,
-    IncrementadorComponent
+    IncrementadorComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(RUTAS)
   ],
   providers: [
     MedicoService
